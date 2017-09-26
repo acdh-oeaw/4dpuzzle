d2b11dc573e501095aa7f9efb70cdfd101cdcf2d
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2017-09-26 14:30:26 +0200

    added helptext to Fielddrawing

--- archiv/models.py
+++ archiv/models.py
@@ -61,26 +61,73 @@ class Foto(models.Model):
 
 
 class Fielddrawing(models.Model):
-    document_id = models.CharField(max_length=500, blank=True)
-    document_type = models.ManyToManyField(SkosConcept, blank=True, related_name="document_type")
+    document_id = models.CharField(
+        max_length=500, blank=True,
+        help_text="This is the unique identifier of the document. It consists of abbreviations for site_document type_inventory number (e.g.: ‘TD_FZ_1234’ means ‘Tell el-Daba_field drawing _1234’)."
+    )
+    document_type = models.ManyToManyField(
+        SkosConcept, blank=True, related_name="document_type",
+        help_text="Definition of resource type (in this case field drawing)"
+    )
     document_subtype = models.ManyToManyField(
-        SkosConcept, blank=True, related_name="document_subtype"
+        SkosConcept, blank=True, related_name="document_subtype",
+        help_text="Definition of the contains of the resource (e.g.: Planum, Profil, Detail, Sondage)"
     )
     scan = models.ManyToManyField(Scan, blank=True)
-    perspective_of_drawing = models.ManyToManyField(SkosConcept, blank=True)
-    stratum_commentary = models.CharField(max_length=300, blank=True)
-    drawn_by = models.ManyToManyField(Person, blank=True)
-    year = models.CharField(max_length=300, blank=True)
-    season = models.ManyToManyField(SkosConcept, blank=True, related_name="season")
-    month = models.ManyToManyField(SkosConcept, blank=True, related_name="month")
-    scale = models.ManyToManyField(SkosConcept, blank=True, related_name="scale")
-    paper_type = models.ManyToManyField(SkosConcept, blank=True, related_name="paper_type")
-    archobject = models.ManyToManyField(ArchObject, blank=True)
-    exobject = models.ManyToManyField(ExObject, blank=True, related_name="exobject_fielddrawing")
-    site = models.ForeignKey(Site, blank=True, null=True)
-    area = models.ForeignKey(Area, blank=True, null=True)
-    square_trence = models.ManyToManyField(SquareTrench, blank=True)
-    planum = models.ManyToManyField(Planum, blank=True)
+    perspective_of_drawing = models.ManyToManyField(
+        SkosConcept, blank=True,
+        help_text="Compass direction of drawer"
+    )
+    stratum_commentary = models.CharField(
+        max_length=300, blank=True,
+        help_text="Exact copy of the stratum commentary on the field drawing"
+    )
+    drawn_by = models.ManyToManyField(
+        Person, blank=True,
+        help_text="Autor/drawer"
+    )
+    year = models.CharField(
+        max_length=300, blank=True, help_text="Year of the production of the field drawing"
+    )
+    season = models.ManyToManyField(
+        SkosConcept, blank=True, related_name="season",
+        help_text="Season of the production of the field drawing"
+    )
+    month = models.ManyToManyField(
+        SkosConcept, blank=True, related_name="month",
+        help_text="Month of the production of the field drawing"
+    )
+    scale = models.ManyToManyField(
+        SkosConcept, blank=True, related_name="scale", help_text="Drawing scale"
+    )
+    paper_type = models.ManyToManyField(
+        SkosConcept, blank=True, related_name="paper_type",
+        help_text="Paper type e.g. graph paper, transparent paper, copy paper"
+    )
+    archobject = models.ManyToManyField(
+        ArchObject, blank=True,
+        help_text="Archaeological object ID contains: Abbreviation of site_area_square trench_description of archaeological object (e.g.: TD_F-I_o19_Grab1, TD_F-I_o19_Opfergrube4)",
+        related_name="archobject_fielddrawing"
+    )
+    exobject = models.ManyToManyField(
+        ExObject, blank=True, related_name="exobject_fielddrawing",
+        help_text="Excavation object ID contains: Abbreviation of site_area_square trench_description of excavation object (e.g.: TD_F-I_o19_Planum1, TD_F-I_o19_Westprofil)"
+    )
+    site = models.ForeignKey(
+        Site, blank=True, null=True,
+        help_text="Archaeological site (here: Tell el Daba – TD)"
+    )
+    area = models.ForeignKey(
+        Area, blank=True, null=True,
+        help_text="Excavation area e.g.: F-I, A-II, H-III"
+    )
+    square_trence = models.ManyToManyField(
+        SquareTrench, blank=True,
+        help_text="Name of the square trench of the excavation area, taken from grid system e.g.: o21, k19"
+    )
+    planum = models.ManyToManyField(
+        Planum, blank=True, help_text="Number of the planum (e.g.: Planum1, Planum2)"
+    )
 
     def __str__(self):
         return "{}".format(self.document_id)
