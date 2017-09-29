71af76271cb81df68b64dfabd0aec1201638c774
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2017-09-29 10:33:38 +0200

    updated models for Find, Brick, Stratunit, added FindSpot model

--- archobs/load.py
+++ archobs/load.py
@@ -1,19 +1,16 @@
 import os
 from django.contrib.gis.utils import LayerMapping
-from .models import Find, Brick, Stratunit
+from .models import *
 
-# Auto-generated `LayerMapping` dictionary for Finds model
-find_mapping = {
+findspot_mapping = {
     'objectid': 'OBJECTID',
-    'gisfind_id': 'GISfind_ID',
+    'orea_gis_i': 'OREA_GIS_I',
     'excavation': 'Excavation',
     'stratum_id': 'Stratum_ID',
     'stratum_gi': 'Stratum_GI',
     'phase_id': 'Phase_ID',
     'phase_gis_field': 'Phase_GIS_',
     'archaeolog': 'Archaeolog',
-    'gis_locus_field': 'GIS_locus_',
-    'gis_su_id': 'GIS_SU_ID',
     'gis_commen': 'GIS_commen',
     'find_mater': 'Find_mater',
     'find_type': 'Find_type',
@@ -21,15 +18,55 @@ find_mapping = {
     'find_inven': 'Find_inven',
     'find_local': 'Find_local',
     'eigner_map': 'Eigner_Map',
-    'resource_i': 'resource_I',
+    'comments': 'Comments',
+    'resources_field': 'Resources_',
     'geom': 'MULTIPOINT',
+}
+findspot_shp = os.path.abspath(
+    os.path.join(
+        os.path.dirname(__file__), 'data', 'shapes',
+        'shp_fuer_ACDH', 'Findspots_20170928.shp'
+    ),
+)
+
+
+def run_findspot(verbose=True):
+    lm = LayerMapping(
+        FindSpot, findspot_shp, findspot_mapping,
+        transform=True, encoding='utf-8',
+    )
+    lm.save(strict=True, verbose=verbose)
+
+find_mapping = {
+    'objectid': 'OBJECTID',
+    'orea_gis_i': 'OREA_GIS_I',
+    'find_mater': 'Find_mater',
+    'find_type': 'Find_type',
+    'excavation': 'Excavation',
+    'height': 'Height',
+    'function': 'Function',
+    'archaeolog': 'Archaeolog',
+    'extrusion': 'Extrusion',
+    'base_heigh': 'Base_heigh',
+    'find_local': 'Find_local',
+    'find_inven': 'Find_inven',
+    'stratum_id': 'Stratum_ID',
+    'stratum_gi': 'Stratum_GI',
+    'phase_id': 'Phase_ID',
+    'phase_gis_field': 'Phase_GIS_',
+    'gis_commen': 'GIS_Commen',
+    'eigner_map': 'Eigner_Map',
+    'shape_leng': 'Shape_Leng',
+    'shape_area': 'Shape_Area',
+    'resources_field': 'Resources_',
+    'geom': 'MULTIPOLYGON',
 }
 
 
 finds_shp = os.path.abspath(
     os.path.join(
         os.path.dirname(__file__), 'data', 'shapes',
-        'converted_finds.shp'
+        'shp_fuer_ACDH', 'Finds_20170928.shp'
     ),
 )
 
@@ -37,58 +74,54 @@ finds_shp = os.path.abspath(
 def run_finds(verbose=True):
     lm = LayerMapping(
         Find, finds_shp, find_mapping,
-        transform=False, encoding='iso-8859-1',
+        transform=True, encoding='utf-8',
     )
     lm.save(strict=True, verbose=verbose)
 
 
 stratunit_mapping = {
     'objectid': 'OBJECTID',
-    'type': 'Type',
-    'gis_su_id': 'GIS_SU_ID',
+    'orea_gis_i': 'OREA_GIS_I',
     'excavation': 'Excavation',
     'stratum_id': 'Stratum_ID',
-    'stratum_gi': 'Stratum_GI',
     'phase_id': 'Phase_ID',
-    'phase_gis_field': 'Phase_GIS_',
     'locus_id': 'Locus_ID',
+    'locus_wall': 'Locus_wall',
     'archaeolog': 'Archaeolog',
     'archaeol_1': 'Archaeol_1',
-    'archaeol_2': 'archaeol_2',
+    'archaeol_2': 'Archaeol_2',
     'height_top': 'Height_top',
-    'height_t_1': 'height_t_1',
+    'height_t_1': 'Height_t_1',
     'height_bot': 'Height_bot',
     'height_b_1': 'Height_b_1',
     'extrusion': 'Extrusion',
-    'base_heigh': 'base_heigh',
+    'base_heigh': 'Base_heigh',
     'eigner_map': 'Eigner_map',
-    'resources_field': 'resources_',
-    'pit_fill': 'pit_fill',
-    'pit_find_g': 'pit_find_G',
-    'bp_materia': 'bp_materia',
-    'bp_door_st': 'bp_door_st',
-    'bp_door_1': 'bp_door__1',
-    'wall_type': 'wall_type',
-    'wall_brick': 'wall_brick',
-    'wall_inter': 'wall_inter',
-    'wall_bri_1': 'wall_bri_1',
-    'locus_laye': 'locus_laye',
+    'pit_fill': 'Pit_fill',
+    'pit_find_g': 'Pit_find_G',
+    'bp_materia': 'Bp_materia',
+    'bp_door_st': 'Bp_door_st',
+    'bp_door_1': 'Bp_door__1',
+    'wall_type': 'Wall_type',
+    'wall_cours': 'Wall_cours',
+    'wall_inter': 'Wall_inter',
+    'wall_brick': 'Wall_brick',
+    'locus_laye': 'Locus_laye',
     'shape_clas': 'Shape_clas',
     'orientatio': 'Orientatio',
-    'stratum': 'Stratum',
-    'phase': 'Phase',
-    'wall_funct': 'wall_funct',
-    'su_nummer': 'SU_Nummer',
-    'wall_conne': 'wall_conne',
-    'old_object': 'old_object',
+    'wall_funct': 'Wall_funct',
+    'wall_conne': 'Wall_conne',
     'shape_leng': 'SHAPE_Leng',
     'shape_area': 'SHAPE_Area',
+    'resources_field': 'Resources_',
     'geom': 'MULTIPOLYGON',
 }
 
+
 stratunit_shp = os.path.abspath(
     os.path.join(
-        os.path.dirname(__file__), 'data', 'shapes', 'converted_stratunits.shp'
+        os.path.dirname(__file__), 'data', 'shapes',
+        'shp_fuer_ACDH', 'Archaeological_objects_20170928.shp'
     ),
 )
 
@@ -96,40 +129,36 @@ stratunit_shp = os.path.abspath(
 def run_strat(verbose=True):
     lm = LayerMapping(
         Stratunit, stratunit_shp, stratunit_mapping,
-        transform=False, encoding='iso-8859-1',
+        transform=True, encoding='utf-8',
     )
     lm.save(strict=True, verbose=verbose)
 
-# Auto-generated `LayerMapping` dictionary for Bricks model
+
 brick_mapping = {
     'objectid': 'OBJECTID',
-    'gisbrick_i': 'GISbrick_I',
+    'orea_gis_i': 'OREA_GIS_I',
     'excavation': 'Excavation',
     'stratum_id': 'Stratum_ID',
-    'stratum_gi': 'Stratum_GI',
     'phase_id': 'Phase_ID',
-    'phase_gis_field': 'Phase_GIS_',
     'archaeolog': 'Archaeolog',
     'archaeol_1': 'Archaeol_1',
     'archaeol_2': 'Archaeol_2',
-    'gis_su_id': 'GIS_SU_ID',
-    'gis_locus_field': 'GIS_locus_',
-    'brick_type': 'brick_type',
-    'brick_mate': 'brick_mate',
+    'brick_type': 'Brick_type',
+    'brick_mate': 'Brick_mate',
     'height_max': 'Height_max',
     'extrusion': 'Extrusion',
+    'base_heigh': 'Base_heigh',
     'orientatio': 'Orientatio',
-    'resource_i': 'Resource_I',
-    'base_heigh': 'base_heigh',
     'shape_leng': 'SHAPE_Leng',
     'shape_area': 'SHAPE_Area',
+    'resources_field': 'Resources_',
     'geom': 'MULTIPOLYGON',
 }
 
 bricks_shp = os.path.abspath(
     os.path.join(
-        os.path.dirname(__file__), 'data', 'shapes',
-        'converted_bricks.shp'
+        os.path.dirname(__file__),  'data', 'shapes',
+        'shp_fuer_ACDH', 'Bricks_20170928.shp'
     ),
 )
 
@@ -137,6 +166,8 @@ bricks_shp = os.path.abspath(
 def run_brick(verbose=True):
     lm = LayerMapping(
         Brick, bricks_shp, brick_mapping,
-        transform=False, encoding='iso-8859-1',
+        transform=True, encoding='utf-8',
     )
     lm.save(strict=True, verbose=verbose)
+
+# C:\Users\pandorfer\ownCloud\GIT\Redmine\4dpuzzle>python manage.py ogrinspect archobs/data/shapes/shp_fuer_ACDH/Bricks_20170928.shp Brick --srid=4326 --mapping --multi >hansi.py --settings=4dpuzzle.settings.pg_local
--- archobs/models.py
+++ archobs/models.py
@@ -77,24 +77,68 @@ class Planum(models.Model):
 
 
 class Find(models.Model):
-    objectid = models.FloatField(blank=True)
-    gisfind_id = models.CharField(blank=True, max_length=250)
-    excavation = models.CharField(blank=True, max_length=250)
-    stratum_id = models.CharField(blank=True, max_length=250)
-    stratum_gi = models.CharField(blank=True, max_length=250)
-    phase_id = models.CharField(blank=True, max_length=250)
-    phase_gis_field = models.CharField(blank=True, max_length=250)
-    archaeolog = models.CharField(blank=True, max_length=250)
-    gis_locus_field = models.CharField(blank=True, max_length=250)
-    gis_su_id = models.CharField(blank=True, max_length=250)
-    gis_commen = models.CharField(blank=True, max_length=250)
-    find_mater = models.CharField(blank=True, max_length=250)
-    find_type = models.CharField(blank=True, max_length=250)
-    height = models.FloatField(blank=True)
-    find_inven = models.CharField(blank=True, max_length=250)
-    find_local = models.CharField(blank=True, max_length=250)
-    eigner_map = models.CharField(blank=True, max_length=250)
-    resource_i = models.CharField(blank=True, max_length=250)
+    objectid = models.IntegerField(blank=True, null=True)
+    orea_gis_i = models.CharField(blank=True, null=True, max_length=254)
+    find_mater = models.CharField(blank=True, null=True, max_length=254)
+    find_type = models.CharField(blank=True, null=True, max_length=254)
+    excavation = models.CharField(blank=True, null=True, max_length=254)
+    height = models.FloatField(blank=True, null=True)
+    function = models.CharField(blank=True, null=True, max_length=254)
+    archaeolog = models.CharField(blank=True, null=True, max_length=254)
+    extrusion = models.FloatField(blank=True, null=True)
+    base_heigh = models.FloatField(blank=True, null=True)
+    find_local = models.CharField(blank=True, null=True, max_length=254)
+    find_inven = models.CharField(blank=True, null=True, max_length=254)
+    stratum_id = models.CharField(blank=True, null=True, max_length=254)
+    stratum_gi = models.CharField(blank=True, null=True, max_length=254)
+    phase_id = models.CharField(blank=True, null=True, max_length=254)
+    phase_gis_field = models.CharField(blank=True, null=True, max_length=254)
+    gis_commen = models.CharField(blank=True, null=True, max_length=254)
+    eigner_map = models.CharField(blank=True, null=True, max_length=254)
+    shape_leng = models.FloatField(blank=True, null=True)
+    shape_area = models.FloatField(blank=True, null=True)
+    resources_field = models.CharField(max_length=254, blank=True, null=True)
+    geom = models.MultiPolygonField(srid=4326)
+
+    def iiifjson(self):
+        return "https://4dpuzzle-iiif.acdh.oeaw.ac.at/{}__{}/info.json".format(
+            self.resource_i, self.excavation
+        ).replace(' ', '').replace('lanum_', 'lanum')
+
+    def __str__(self):
+        return "{}".format(self.orea_gis_i)
+
+    def get_next(self):
+        next = Find.objects.filter(id__gt=self.id)
+        if next:
+            return next.first().id
+        return False
+
+    def get_prev(self):
+        prev = Find.objects.filter(id__lt=self.id).order_by('-id')
+        if prev:
+            return prev.first().id
+        return False
+
+
+class FindSpot(models.Model):
+    objectid = models.IntegerField(blank=True, null=True)
+    orea_gis_i = models.CharField(blank=True, null=True, max_length=254)
+    excavation = models.CharField(blank=True, null=True, max_length=254)
+    stratum_id = models.CharField(blank=True, null=True, max_length=254)
+    stratum_gi = models.CharField(blank=True, null=True, max_length=254)
+    phase_id = models.CharField(blank=True, null=True, max_length=254)
+    phase_gis_field = models.CharField(blank=True, null=True, max_length=254)
+    archaeolog = models.CharField(blank=True, null=True, max_length=254)
+    gis_commen = models.CharField(blank=True, null=True, max_length=254)
+    find_mater = models.CharField(blank=True, null=True, max_length=254)
+    find_type = models.CharField(blank=True, null=True, max_length=254)
+    height = models.FloatField(blank=True, null=True)
+    find_inven = models.CharField(blank=True, null=True, max_length=254)
+    find_local = models.CharField(blank=True, null=True, max_length=254)
+    eigner_map = models.CharField(blank=True, null=True, max_length=254)
+    comments = models.CharField(blank=True, null=True, max_length=254)
+    resources_field = models.CharField(blank=True, null=True, max_length=254)
     geom = models.MultiPointField(srid=4326)
 
     def iiifjson(self):
@@ -103,7 +147,7 @@ class Find(models.Model):
         ).replace(' ', '').replace('lanum_', 'lanum')
 
     def __str__(self):
-        return "{}".format(self.gisfind_id)
+        return "{}".format(self.orea_gis_i)
 
     def get_next(self):
         next = Find.objects.filter(id__gt=self.id)
@@ -119,27 +163,23 @@ class Find(models.Model):
 
 
 class Brick(models.Model):
-    objectid = models.FloatField(blank=True, null=True)
-    gisbrick_i = models.CharField(blank=True, max_length=250)
-    excavation = models.CharField(blank=True, max_length=250)
-    stratum_id = models.CharField(blank=True, max_length=250)
-    stratum_gi = models.CharField(blank=True, max_length=250)
-    phase_id = models.CharField(blank=True, max_length=250)
-    phase_gis_field = models.CharField(blank=True, max_length=250)
-    archaeolog = models.CharField(blank=True, max_length=250)
-    archaeol_1 = models.CharField(blank=True, max_length=250)
-    archaeol_2 = models.CharField(blank=True, max_length=250)
-    gis_su_id = models.CharField(blank=True, max_length=250)
-    gis_locus_field = models.CharField(blank=True, max_length=250)
-    brick_type = models.CharField(blank=True, max_length=250)
-    brick_mate = models.CharField(blank=True, max_length=250)
+    objectid = models.IntegerField(blank=True, null=True)
+    orea_gis_i = models.CharField(blank=True, null=True, max_length=254)
+    excavation = models.CharField(blank=True, null=True, max_length=254)
+    stratum_id = models.CharField(blank=True, null=True, max_length=254)
+    phase_id = models.CharField(blank=True, null=True, max_length=254)
+    archaeolog = models.CharField(blank=True, null=True, max_length=254)
+    archaeol_1 = models.CharField(blank=True, null=True, max_length=254)
+    archaeol_2 = models.CharField(blank=True, null=True, max_length=254)
+    brick_type = models.CharField(blank=True, null=True, max_length=254)
+    brick_mate = models.CharField(blank=True, null=True, max_length=254)
     height_max = models.FloatField(blank=True, null=True)
     extrusion = models.FloatField(blank=True, null=True)
-    orientatio = models.CharField(blank=True, max_length=250)
-    resource_i = models.CharField(blank=True, max_length=250)
     base_heigh = models.FloatField(blank=True, null=True)
+    orientatio = models.CharField(blank=True, null=True, max_length=254)
     shape_leng = models.FloatField(blank=True, null=True)
     shape_area = models.FloatField(blank=True, null=True)
+    resources_field = models.CharField(blank=True, null=True, max_length=254)
     geom = models.MultiPolygonField(srid=4326)
 
     def iiifjson(self):
@@ -160,54 +200,48 @@ class Brick(models.Model):
         return False
 
     def __str__(self):
-        return "{}".format(self.gisbrick_i)
+        return "{}".format(self.orea_gis_i)
 
 
 class Stratunit(models.Model):
-    objectid = models.FloatField(blank=True, null=True)
-    type = models.CharField(blank=True, max_length=250)
-    gis_su_id = models.CharField(blank=True, max_length=250)
-    excavation = models.CharField(blank=True, max_length=250)
-    stratum_id = models.CharField(blank=True, max_length=250)
-    stratum_gi = models.CharField(blank=True, max_length=250)
-    phase_id = models.CharField(blank=True, max_length=250)
-    phase_gis_field = models.CharField(blank=True, max_length=250)
-    locus_id = models.CharField(blank=True, max_length=250)
-    archaeolog = models.CharField(blank=True, max_length=250)
-    archaeol_1 = models.CharField(blank=True, max_length=250)
-    archaeol_2 = models.CharField(blank=True, max_length=250)
+    objectid = models.IntegerField(blank=True, null=True)
+    orea_gis_i = models.CharField(blank=True, null=True, max_length=254)
+    excavation = models.CharField(blank=True, null=True, max_length=254)
+    stratum_id = models.CharField(blank=True, null=True, max_length=254)
+    phase_id = models.CharField(blank=True, null=True, max_length=254)
+    locus_id = models.CharField(blank=True, null=True, max_length=254)
+    locus_wall = models.CharField(blank=True, null=True, max_length=254)
+    archaeolog = models.CharField(blank=True, null=True, max_length=254)
+    archaeol_1 = models.CharField(blank=True, null=True, max_length=254)
+    archaeol_2 = models.CharField(blank=True, null=True, max_length=254)
     height_top = models.FloatField(blank=True, null=True)
-    height_t_1 = models.FloatField(blank=True, null=True)
-    height_bot = models.FloatField(blank=True, null=True)
-    height_b_1 = models.FloatField(blank=True, null=True)
+    height_t_1 = models.CharField(blank=True, null=True, max_length=254)
+    height_bot = models.CharField(blank=True, null=True, max_length=254)
+    height_b_1 = models.CharField(blank=True, null=True, max_length=254)
     extrusion = models.FloatField(blank=True, null=True)
     base_heigh = models.FloatField(blank=True, null=True)
-    eigner_map = models.CharField(blank=True, max_length=250)
-    resources_field = models.CharField(blank=True, max_length=250)
-    pit_fill = models.CharField(blank=True, max_length=250)
-    pit_find_g = models.CharField(blank=True, max_length=250)
-    bp_materia = models.CharField(blank=True, max_length=250)
-    bp_door_st = models.CharField(blank=True, max_length=250)
-    bp_door_1 = models.CharField(blank=True, max_length=250)
-    wall_type = models.CharField(blank=True, max_length=250)
-    wall_brick = models.CharField(blank=True, max_length=250)
-    wall_inter = models.CharField(blank=True, max_length=250)
-    wall_bri_1 = models.CharField(blank=True, max_length=250)
-    locus_laye = models.CharField(blank=True, max_length=250)
-    shape_clas = models.CharField(blank=True, max_length=250)
-    orientatio = models.CharField(blank=True, max_length=250)
-    stratum = models.CharField(blank=True, max_length=250)
-    phase = models.CharField(blank=True, max_length=250)
-    wall_funct = models.CharField(blank=True, max_length=250)
-    su_nummer = models.CharField(blank=True, max_length=250)
-    wall_conne = models.CharField(blank=True, max_length=250)
-    old_object = models.CharField(blank=True, max_length=250)
+    eigner_map = models.CharField(blank=True, null=True, max_length=254)
+    pit_fill = models.CharField(blank=True, null=True, max_length=254)
+    pit_find_g = models.CharField(blank=True, null=True, max_length=254)
+    bp_materia = models.CharField(blank=True, null=True, max_length=254)
+    bp_door_st = models.CharField(blank=True, null=True, max_length=254)
+    bp_door_1 = models.CharField(blank=True, null=True, max_length=254)
+    wall_type = models.CharField(blank=True, null=True, max_length=254)
+    wall_cours = models.CharField(blank=True, null=True, max_length=254)
+    wall_inter = models.CharField(blank=True, null=True, max_length=254)
+    wall_brick = models.CharField(blank=True, null=True, max_length=254)
+    locus_laye = models.CharField(blank=True, null=True, max_length=254)
+    shape_clas = models.CharField(blank=True, null=True, max_length=254)
+    orientatio = models.CharField(blank=True, null=True, max_length=254)
+    wall_funct = models.CharField(blank=True, null=True, max_length=254)
+    wall_conne = models.CharField(blank=True, null=True, max_length=254)
     shape_leng = models.FloatField(blank=True, null=True)
     shape_area = models.FloatField(blank=True, null=True)
+    resources_field = models.CharField(blank=True, null=True, max_length=254)
     geom = models.MultiPolygonField(srid=4326)
 
     def __str__(self):
-        return "{}".format(self.gis_su_id)
+        return "{}".format(self.orea_gis_i)
 
     def iiifjson(self):
         return "https://4dpuzzle-iiif.acdh.oeaw.ac.at/{}__{}/info.json".format(
