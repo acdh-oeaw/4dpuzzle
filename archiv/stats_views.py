13d16a2f409a641f693383798a2b816ba5e1cfe0
csae8092 <peter.andorfer@oeaw.ac.at>
2019-12-17 16:23:57 +0100

    added match-binaries view

---
+++ archiv/stats_views.py
@@ -0,0 +1,38 @@
+from django.views.generic import TemplateView
+
+from webpage.appcreator.import_utils import fetch_models
+
+from . models import *
+
+
+class MatchBinaryView(TemplateView):
+    template_name = 'archiv/match_stats.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        summary = []
+        for x in fetch_models('archiv'):
+            class_name = x.__name__
+            class_list_view = x.get_listview_url()
+            items = x.objects.all()
+            all_objects = items.count()
+            match = items.exclude(fc_match=False).count()
+            no_match = items.exclude(fc_match=True).count()
+            try:
+                percentage = (match / all_objects) * 100
+            except Exception as e:
+                percentage = 0
+            summary.append(
+                [
+                    class_name,
+                    all_objects,
+                    match,
+                    no_match,
+                    percentage
+                ]
+            )
+            context['table_head'] = [
+                'class_name', 'all_objects', 'match', 'no_match', 'percentage'
+            ]
+            context['table_body'] = summary
+        return context
