8532015046c3a340a11952207b1a9ba0c722dd68
csae8092 <peter.andorfer@oeaw.ac.at>
2020-11-05 15:25:49 +0100

    pagination added archeutils/archiv/fielddrawing?start=0&page_size=100

--- archeutils/arche_rdf_views.py
+++ archeutils/arche_rdf_views.py
@@ -49,14 +49,22 @@ def res_as_arche_graph(request, app_name, model_name, pk):
 
 
 def qs_as_arche_graph(request, app_name, model_name):
-    start = 0
-    page_size = 100
+    try:
+        start = int(request.GET.get('start', 0))
+    except ValueError:
+        start = 0
+    try:
+        page_size = int(request.GET.get('page_size', 100))
+    except ValueError:
+        page_size = 100
+
+    page_size = start + page_size
     try:
         ct = ContentType.objects.get(app_label=app_name, model=model_name)
     except ObjectDoesNotExist:
         raise Http404(f"No model: {model_name} in app: {app_name} defined")
-    qs = ct.model_class().objects.all()
-    maingraph = qs_as_arche_res(qs[start:page_size])
+    qs = ct.model_class().objects.all()[start:page_size]
+    maingraph = qs_as_arche_res(qs)
     return HttpResponse(
         maingraph.serialize(encoding='utf-8', format='turtle'),
         content_type='text/turtle'
@@ -73,7 +81,7 @@ def relevante_classes(request, app_name):
             item = {
                 "class_name": f"{x.__name__.lower()}",
                 "object_count": x.objects.all().count(),
-                "endpoint": f"{base_uri}/archeutils/{app_name}/{x.__name__.lower()}"
+                "endpoint": f"{base_uri}/archeutils/{app_name}/{x.__name__.lower()}?start=0&page_size=100"
             }
             good_models.append(item)
         except AttributeError:
