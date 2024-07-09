8f98644e7e7d2289bdbd52e9d01cf12a87050c56
csae8092 <peter.andorfer@oeaw.ac.at>
2024-07-09 14:52:44 +0200

    more tests

--- create_fixtures.sh
+++ create_fixtures.sh
@@ -4,9 +4,17 @@
 # make sure you ran `pip install django-fixture-magic` and added `'fixture_magic'` to INSTALLED_APPS
 source env/bin/activate
 source set_env_variables.sh
-mkdir ./tablets/fixtures
-touch ./tablets/fixtures/dump.json
-echo "create fixtures_courtdecission"
-python manage.py dump_object tablets.tablet 425 542 > ./tablets/fixtures/dump.json
+mkdir ./archiv/fixtures
+touch ./archiv/fixtures/dump.json
+echo "create fixtures_archiv"
+python manage.py dump_object archiv.fielddrawing 1557 > ./archiv/fixtures/fielddrawing.json
+python manage.py dump_object archiv.fotosgescannt 8 > ./archiv/fixtures/fotosgescannt.json
+
+echo "merging fixtures"
+python manage.py merge_fixtures ./archiv/fixtures/fielddrawing.json ./archiv/fixtures/fotosgescannt.json > archiv/fixtures/dump.json
+
+rm ./archiv/fixtures/fielddrawing.json
+rm ./archiv/fixtures/fotosgescannt.json
+
 
 echo "done"
