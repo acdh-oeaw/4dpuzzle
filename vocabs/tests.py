5e9bd836bcc72a2f01f060e74735aab90ec2d530
csae8092 <peter.andorfer@oeaw.ac.at>
2024-07-09 13:49:15 +0200

    adds missing migrations; fixed existing tests; moved notebook into dedicated folder

--- vocabs/tests.py
+++ vocabs/tests.py
@@ -11,13 +11,13 @@ class VocabsTest(TestCase):
 
     def test_vocabs(self):
         rv = self.client.get("/vocabs/scheme/")
-        self.assertContains(rv, "Concept Schemes")
+        self.assertContains(rv, "Browse Skos Concept Scheme")
         rv = self.client.get("/vocabs/scheme/create/", follow=True)
         self.assertContains(rv, "Namespace")
 
     def test_concept_schema_detail(self):
         rv = self.client.get("/vocabs/create/")
-        self.assertContains(rv, "Pref label")
+        self.assertContains(rv, "Skos:prefLabel")
         form_data = {"pref_label": "test concept"}
-        self.client.post("/vocabs/create/", form_data, follow=True)
-        self.assertContains(rv, "Skos broadmatch")
+        rv = self.client.post("/vocabs/create/", form_data, follow=True)
+        self.assertTrue("test concept" in str(rv.content))
